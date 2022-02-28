b8ab0d03ce7d6ac102ec84b02486d87a783228ef
tomasV10 <tomasvarnauskas19@gmail.com>
2022-02-28 16:48:57 +0200

    first commit

---
+++ src/main/java/lt/internal/pdfValidatorApplication/PdfValidatorApplication.java
@@ -0,0 +1,13 @@
+package lt.internal.pdfValidatorApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/internal/pdfValidatorApplication/PdfValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.internal.pdfValidatorApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
